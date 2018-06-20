be3d1b058cb7607524c9663c949b99daeecf0e84
Apple Japan <TsudaJun@tsudajun-no-macbook-air.local>
2018-06-20 23:11:36 +0900

    Initial Commit

---
+++ MemoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 津田準 on 2018/06/20.
+//  Copyright © 2018 津田準. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
